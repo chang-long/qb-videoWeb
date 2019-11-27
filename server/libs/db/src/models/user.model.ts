ef5ecc829a77daa3263d2c286afd8e4b2be8f636
zhengqbbb <1074059947@qq.com>
2019-11-28 00:33:42 +0800

    :sparkles: :memo: Use Swagger and Done APIDoc

--- server/libs/db/src/models/user.model.ts
+++ server/libs/db/src/models/user.model.ts
@@ -1,9 +1,17 @@
-import { prop } from '@typegoose/typegoose';
+import { prop, modelOptions } from '@typegoose/typegoose';
+import { ApiModelProperty } from '@nestjs/swagger';
 
+@modelOptions({
+  schemaOptions: {
+    timestamps: true
+  }
+})
 export class User {
+  @ApiModelProperty({ description: '用户名', example: 'use1' })
   @prop()
   username: string
 
+  @ApiModelProperty({ description: '密码', example: 'pass1' })
   @prop()
   password: string
 }
