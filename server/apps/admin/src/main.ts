1481d458aec3b009cbe472cd470b938c71bf9a94
zhengqbbb <1074059947@qq.com>
2019-12-02 20:17:34 +0800

    :heavy_plus_sign: :wrench: Add Cors config

--- server/apps/admin/src/main.ts
+++ server/apps/admin/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('Qbenben视频全栈网站-后台管理API')
     .setDescription('供后台管理界面调用的服务端')
