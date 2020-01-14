c949e5f5343b9c9c65a8f0fce19e114a8d8cba37
zhengqbbb <1074059947@qq.com>
2020-01-14 23:26:53 +0800

    :heavy_plus_sign: :new: use bcryptjs for  register

--- server/libs/db/src/models/user.model.ts
+++ server/libs/db/src/models/user.model.ts
@@ -1,21 +1,26 @@
 import { prop, modelOptions } from '@typegoose/typegoose';
 import { ApiProperty } from '@nestjs/swagger';
+import { hashSync } from 'bcryptjs';
 
 @modelOptions({
   schemaOptions: {
-    timestamps: true
-  }
+    timestamps: true,
+  },
 })
 export class User {
   @ApiProperty({ description: '用户名', example: 'use1' })
   @prop()
-  username: string
+  username: string;
 
   @ApiProperty({ description: '密码', example: 'pass1' })
   @prop({
-    set(val){
-      return 
-    }
+    select: false,
+    get(val) {
+      return val;
+    },
+    set(val) {
+      return val ? hashSync(val) : val;
+    },
   })
-  password: string
+  password: string;
 }
